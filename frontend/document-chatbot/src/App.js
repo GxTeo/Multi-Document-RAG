176aea333fd607ea7a56a8aaf0a1b84d67f8827a
GxTeo <teog0015@e.ntu.edu.sg>
2024-06-01 19:17:11 +0800

    set props for collection selection

--- frontend/document-chatbot/src/App.js
+++ frontend/document-chatbot/src/App.js
@@ -1,17 +1,28 @@
-// src/App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import FileUpload from './components/FileUpload';
 import Chatbot from './components/Chatbot';
 
 function App() {
+  const [collections, setCollections] = useState(() => {
+    const savedCollections = localStorage.getItem('collections');
+    return savedCollections ? JSON.parse(savedCollections) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('collections', JSON.stringify(collections));
+  }, [collections]);
+
+  const handleNewCollection = (collectionName) => {
+    setCollections([...collections, collectionName]);
+  };
   return (
     <div className="App">
       <div className="left">
-        <FileUpload />
+        <FileUpload onNewCollection={handleNewCollection} />
       </div>
       <div className="right">
-        <Chatbot />
+        <Chatbot collections={collections} />
       </div>
     </div>
   );
