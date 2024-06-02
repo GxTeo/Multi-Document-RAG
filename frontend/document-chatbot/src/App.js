cb4ba1cd04230da2d6ca910e06315f2739408dfb
GxTeo <teog0015@e.ntu.edu.sg>
2024-06-02 17:10:39 +0800

    added fectch collection function to toggle update across components

--- frontend/document-chatbot/src/App.js
+++ frontend/document-chatbot/src/App.js
@@ -1,17 +1,34 @@
 // src/App.js
-import React, { useState } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import './App.css';
 import FileUpload from './components/FileUpload';
+import CollectionsDisplay from './components/CollectionsDisplay'; // Import the CollectionsDisplay component
 import Chatbot from './components/Chatbot';
 
 function App() {
+  const fetchCollectionsRef = useRef(null);
+  const [updateToggle, setUpdateToggle] = useState(false); // To trigger re-fetches
+
+  const setFetchCollections = (fetchCollectionsFunction) => {
+    fetchCollectionsRef.current = fetchCollectionsFunction;
+  };
+
+  // Wrapper around fetchCollections to allow forcing update from any component
+  const handleFetchCollections = useCallback(() => {
+    if (fetchCollectionsRef.current) {
+      fetchCollectionsRef.current();
+      setUpdateToggle(prev => !prev); // Toggle to force rerender
+    }
+  }, []);
+
   return (
     <div className="App">
       <div className="left">
-        <FileUpload />
+        <FileUpload fetchCollections={handleFetchCollections} />
+        <CollectionsDisplay key={updateToggle} />
       </div>
       <div className="right">
-        <Chatbot />
+      <Chatbot setFetchCollections={setFetchCollections} handleFetchCollections={handleFetchCollections} />      
       </div>
     </div>
   );
