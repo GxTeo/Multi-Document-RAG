787217a0b3dd05dd37c3759a3fa5aff8b2493d11
GxTeo <teog0015@e.ntu.edu.sg>
2024-07-06 02:17:39 +0800

    multi-user authentication script

---
+++ backend/jwttoken.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+import os
+
+import main
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+def verify_token(token:str,credentials_exception):
+ try:
+     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+     username: str = payload.get("sub")
+     if username is None:
+         raise credentials_exception
+     token_data = main.TokenData(username=username)
+ except JWTError:
+     raise credentials_exception
+ 
+ # return the User object
+ user = main.get_user(username=token_data.username)
+ print('User object: ',user)
+ return user
---
+++ backend/oauth.py
@@ -0,0 +1,14 @@
+from fastapi import Depends,HTTPException
+from jwttoken import verify_token
+from fastapi.security import OAuth2PasswordBearer
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+from fastapi import status
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+ 
+ credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+ return verify_token(token,credentials_exception)
